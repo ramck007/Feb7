89bd8723b37248abfd8dd4e4fe4de02bc92ecd91
ramck007 <98875517+ramck007@users.noreply.github.com>
2022-02-07 21:09:59 +0530

    Add files via upload

---
+++ Task24.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Task24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y=10;
+		int z=(++y*(y++ +5));
+		System.out.println(z);
+				
+	}
+
+}
